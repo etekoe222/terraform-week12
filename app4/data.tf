58bab8630e8aaa33e598c3418ad2f9d177bd03db
edem22 <edemtekoe@gmail.com>
2023-11-22 21:38:40 -0600

    week12

---
+++ app4/data.tf
@@ -0,0 +1,27 @@
+data "aws_instance" "demo" {
+  instance_id = "i-091e0ca82f3c9555e"
+
+}
+
+data "aws_ami" "ami1" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
---
+++ app5/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_instance.demo
+  id = "i-091e0ca82f3c9555e"
+}
